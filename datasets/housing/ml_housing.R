c5bc8d0d339ecf3cd5698a9a835588cfa62236d4
hcb1206 <hcb1206@gmail.com>
2021-01-05 13:55:50 -0500

    finishing the example script

--- datasets/housing/ml_housing.R
+++ datasets/housing/ml_housing.R
@@ -3,7 +3,7 @@
 library(tidyverse)
 library(reshape2)
 
-setwd("~/R/machinelearning_housing/handson-ml-master/datasets/housing")
+setwd("~/R/github/handson-ml/datasets/housing")
 housing <- read.csv("housing.csv")
 
 View(housing)
@@ -89,4 +89,68 @@ k_fold_cv_error <- cv.glm(cleaned_housing, glm_house, K=5)
 k_fold_cv_error$delta #The first component is the raw cross-validation estimate of 
                       #prediction error. The second component is the adjusted 
                       #cross-validation estimate.
+glm_cv_rmse <- sqrt(k_fold_cv_error$delta)[1]
+glm_cv_rmse #this number is apparently off, but I don't know from what.
 
+names(glm_house) #what parts of the model are callable?
+
+#okay, new model. this one is random forest.
+
+library(randomForest)
+names(train)
+
+set.seed(1738)
+
+train_y <- train[ , 'median_house_value']
+train_x <- train[ , names(train) !='median_house_value']
+
+head(train_y)
+head(train_x)
+
+rf_model <- randomForest(train_x, y = train_y, ntree = 500, 
+                         importance = TRUE)
+names(rf_model)
+rf_model$importance #the higher the following %s are, the more 
+                    #important the predictor
+oob_prediction <- predict(rf_model)
+train_mse <- mean(as.numeric((oob_prediction - train_y)^2))
+oob_rmse <- sqrt(train_mse)
+oob_rmse
+
+#the above model can predict the median price of a house within
+#49,000 of the actual median price in the training data, so 
+#it's not too bad. lets run it in the test data now.
+
+test_y = test[, 'median_house_value']
+test_x = test[, names(test) !='median_house_value']
+
+y_pred <- predict(rf_model, test_x)
+test_mse <- mean(((y_pred - test_y)^2))
+test_rmse = sqrt(test_mse)
+test_rmse
+
+#since the model scored roughly the same on the training and testing
+#data, we can assume that the model is not 'overfit' and it will 
+#continue to make good predictons
+
+#THERE IS ROOM FOR IMPROVEMENT IN THIS MODEL
+#SUGGESTED WAYS TO IMPROVE THE PREDICTIONS:
+#   take a closer look at the latitude and longitude, and 
+#    use it to do something called 'feature engineering', like
+#    finding the distance to the closest large city, or other 
+#    location based stats
+#   try out different models that may make better predictions
+#    some examples include other tree based methods, like 
+#    gradient boosting - library(gbm) or 
+#    extreme gradient boosting - library(xgb)
+#    or other fun methods such as
+#    support vector machines - library(e1071) or
+#    neural networks - library(neuralnet)
+
+
+# more at 
+#https://www.kaggle.com/camnugent/introduction-to-machine-learning-in-r-tutorial/notebook
+
+
+    
+    
